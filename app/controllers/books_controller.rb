ec069f6b76df6ab5f7ce6d15a801dedb087c22a6
EC2 Default User <ec2-user@ip-172-31-33-190.ap-northeast-1.compute.internal>
2022-09-09 06:27:35 +0000

    bookers[フォーム作成中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    
+    book.save
+    
+    redirect_to'/top'
+  end
+  
   def index
   end
 
@@ -10,4 +19,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+
+private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'books/new'
-  get 'books/index'
+  get 'books/index' => 'new'
+  post 'books/index' => 'books#create'
   get 'books/show'
   get 'books/edit'
-  get 'top'=>'homes#top'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top'=>'homes#top'#
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
